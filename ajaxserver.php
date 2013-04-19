6808cdba7b797959cae78d6349e932d3e12a8bd8
Morten Sickel <morten@sickel.net>
2013-04-19 21:12:27 +0200

    Rewritten to fetch all temperature data from measure_qc - needs a serious cleanup

--- ajaxserver.php
+++ ajaxserver.php
@@ -19,39 +19,44 @@ try{
     exit( "<p>Cannot connect - $message</p>");
   }
 
-  $sql='select temp as "value",to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from temp_stream where name=? and datetime>?';
-
+#  $sql='select temp as "value",to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from temp_stream where name=? and datetime>?';
+ $sql='select value, to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure_qa where sensorid=? and datetime>?';
 $params=array($_GET['from']);
+if($_GET['stream']=='Inne'){
+   array_unshift($params,2);
+}elseif($_GET['stream']=='Ute'){
+   array_unshift($params,1);
+}
 if($_GET['stream']=='Inne-Ute'){
   $sql="select value,at from tempdiff where datetime >?";
  }elseif($_GET['stream']=='Trykk'){
-  $sql='select value/100 as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure where sensorid=4 and datetime>?';
+  $sql='select value/100 as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure_qa where sensorid=4 and datetime>?';
 //  TODO fetch units from database
   $unit='hPa';
 }elseif($_GET['stream']=='Trykk - 0m'){
-  $sql='select value/100+12*0.45 as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure where sensorid=4 and datetime>?';
+  $sql='select value/100+12*0.45 as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure_qa where sensorid=4 and datetime>?';
   $unit='hPa';
 }elseif($_GET['stream']=='Fuktighet'){
-  $sql='select value as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure where sensorid=5 and datetime>?';
+  $sql='select value as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure_qa where sensorid=5 and datetime>?';
   $unit="%";
 }elseif($_GET['stream']=='Fuktighet DHT22'){
-  $sql='select value as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure where sensorid=9 and datetime>?';
+  $sql='select value as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure_qa where sensorid=9 and datetime>?';
   $unit="%";
 }elseif($_GET['stream']=='Temp DHT22'){
-  $sql='select value as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure where sensorid=8 and datetime>?';
+  $sql='select value as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure_qa where sensorid=8 and datetime>?';
   $unit="&degC";
 }elseif($_GET['stream']=='Temp DHT11'){
-  $sql='select value as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure where sensorid=6 and datetime>?';
+  $sql='select value as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure_qa where sensorid=6 and datetime>?';
   $unit="&degC";
 }elseif($_GET['stream']=='Temp BHP085'){
-  $sql='select value as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure where sensorid=3 and datetime>?';
+  $sql='select value as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from measure_qa where sensorid=3 and datetime>?';
   $unit="&degC";
 }elseif($_GET['stream']=='Forbruk'){
   $sql='select round(100*kwh/hours)/100 as "value", to_char(datetime at time zone \'UTC\' ,\'yyyy-mm-dd"T"HH24:MI:SS"Z"\') as "at" from powerdraw where datetime >?';	
   $unit='kW';
-}else{
-  array_unshift($params,$_GET['stream']);
-}
+}#else{
+ # array_unshift($params,$_GET['stream']);
+#}
 if($_GET['to']*1>1){
 	$params[]=$_GET['to'];
 	$sql.=' and datetime < ?';
@@ -59,7 +64,7 @@ if($_GET['to']*1>1){
 $sql.=' order by datetime';
 // print($sql);
 $sqh=$dbh->prepare($sql);
-	
+# print_r($params);	
 $sqh->execute($params);
 $data=$sqh->fetchAll(PDO::FETCH_ASSOC);
 
@@ -71,6 +76,7 @@ if($_GET['type']=='svg'){
 	$data['debug']['sql']=$sql;
 	$data['debug']['name']=$_GET['stream'];
 	$data['debug']['from']=$_GET['from'];
+	$data['params']=$params;
 }	
 	print('{
   	"datapoints":');
