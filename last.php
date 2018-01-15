bb3f1e3c08d549ad21ece30acab1fd6732431c1c
Morten Sickel <morten@sickel.net>
2018-01-15 21:56:11 +0100

    Added pagetitle - cleaned up

--- last.php
+++ last.php
@@ -1,16 +1,13 @@
 <?php
 
 include('connect_db.php');
-
 require 'smarty3/Smarty.class.php';
-
 $smarty = new Smarty;
-
 //$smarty->force_compile = true;
 //$smarty->debugging = true;
 $smarty->caching = false;
 $smarty->cache_lifetime = 120;
-
+$smarty->assign('pagetitle',"Last measurement");
 
 /*
  The following query will fetch the last measurement for each sensor that is flagged as active
@@ -34,31 +31,10 @@ if(array_key_exists('json',$_GET)){
   header('Content-type: application/json');
   die(json_encode($data));
 }
-//print_r($data);
-
 $showage=array_key_exists('age',$_GET);
-//print('<table class="lastlist">');
 $age=$showage?"<th>alder (min)</th>":"";
-//print("<tr><th>Måling</th><th>Verdi</th>$age</tr>\n");
 $sensors=array_key_exists('s',$_GET)?$_GET['s']:array();
 $smarty->assign('data',$data);
-/*foreach ($data as $s){
-  if($s['main'] || $_GET['showall']){
-    $class=$s['since']< 60*20?'default':'olddata';
-    $s['since']=round($s['since']/60);
-    $txt="<a href=\"stripchart.php?selid=${s['sensorid']}\">${s['type']} ${s['station']}</a>   </td><td class=\"right\"> <b>${s['value']} ${s['unit']}</td>";
-    if($showage){
-      //$txt.="<td class=\"center\">${s['since']}</td>";
-    }
-    if(array_key_exists('showids',$_GET)){
-      $txt="(${s['sensorid']}) $txt";
-    }	
-    print("<tr class=\"$class\"><td class=\"right\">$txt</tr>\n");
-  }
-}
-
-print("</table><hr />");
-*/
 date_default_timezone_set('Europe/Oslo');
 $smarty->assign('vlevel',voltagelevel());
 
